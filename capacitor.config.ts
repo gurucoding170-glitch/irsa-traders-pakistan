2a0105215fb12970e4234aa1775ae9009b3a8488
gurucoding170-glitch <gurucoding170@gmail.com>
2025-08-12 10:18:39 +0000

    created android folder

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.irsatraders.app',
+  appName: 'Irsa Traders Pakistan',
+  webDir: 'dist'
+};
+
+export default config;
